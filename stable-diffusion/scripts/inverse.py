9aad668c9ebb14298159c224994a4f7b52a76478
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-23 16:18:34 -0400

    Moved notebook to script

--- stable-diffusion/scripts/inverse.py
+++ stable-diffusion/scripts/inverse.py
@@ -100,7 +100,7 @@ def main():
     parser.add_argument(
         "--ddim_steps",
         type=int,
-        default=100,
+        default=200,
         help="number of ddim sampling steps",
     )
     parser.add_argument(
@@ -174,7 +174,7 @@ def main():
     parser.add_argument(
         "--scale",
         type=float,
-        default=5,
+        default=3,
         help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
     )
     parser.add_argument(
