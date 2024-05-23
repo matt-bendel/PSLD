24f24e5b5ea4050307f0be602913396ba66e7a46
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-23 14:39:17 -0400

    Moved notebook to script

--- stable-diffusion/scripts/inverse.py
+++ stable-diffusion/scripts/inverse.py
@@ -100,7 +100,7 @@ def main():
     parser.add_argument(
         "--ddim_steps",
         type=int,
-        default=1000,
+        default=100,
         help="number of ddim sampling steps",
     )
     parser.add_argument(
