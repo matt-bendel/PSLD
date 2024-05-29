77ce900589357fead16788676cc3decdcebe251e
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-29 00:53:19 -0400

    Moved notebook to script

--- stable-diffusion/scripts/inverse.py
+++ stable-diffusion/scripts/inverse.py
@@ -453,7 +453,7 @@ def main():
                     else:
                         # pdb.set_trace()
                         if opt.scale != 1.0 :
-                            uc = model.get_learned_conditioning(batch_size * ["A photo of a face"])
+                            uc = model.get_learned_conditioning(batch_size * ["A close-up photograph of a little girl's face"])
                         if isinstance(prompts, tuple):
                             prompts = list(prompts)
 
