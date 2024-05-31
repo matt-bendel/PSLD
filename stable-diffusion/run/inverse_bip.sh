f2ff9b4dafeceb13aa51d932cf4a3434eb3ffd86
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-05-31 13:41:17 -0400

    Moved notebook to script

--- stable-diffusion/run/inverse_bip.sh
+++ stable-diffusion/run/inverse_bip.sh
@@ -4,6 +4,6 @@ python scripts/inverse.py \
     --task_config='configs/box_inpainting_config_psld.yaml' \
     --inpainting=1 \
     --general_inverse=0 \
-    --gamma=5e-2 \
-    --omega=5e-1 \
+    --gamma=1e-2 \
+    --omega=1e-1 \
     --outdir='outputs/psld-samples-bip' 
