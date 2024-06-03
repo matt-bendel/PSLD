3cd3787f1ab50280052f3d4d7080df4f5e281f52
Matt Bendel <mattbendel@Matts-MacBook-Pro-3.local>
2024-06-03 15:37:14 -0400

    Moved notebook to script

--- stable-diffusion/ldm/models/diffusion/psld.py
+++ stable-diffusion/ldm/models/diffusion/psld.py
@@ -20,7 +20,7 @@ class DDIMSampler(object):
         # TODO
         self.optimal_c = None
         self.opt = None
-        self.K = 1
+        self.K = 2
 
     def register_buffer(self, name, attr):
         if type(attr) == torch.Tensor:
